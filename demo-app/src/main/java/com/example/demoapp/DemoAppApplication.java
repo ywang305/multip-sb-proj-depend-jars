837e1632db6a1cb7b7f83753f35716fdf0d9a612
yao <ywang305@syr.edu>
2021-02-25 23:40:49 -0500

    multi modlues

--- demo-app/src/main/java/com/example/demoapp/DemoAppApplication.java
+++ demo-app/src/main/java/com/example/demoapp/DemoAppApplication.java
@@ -3,18 +3,14 @@ package com.example.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Service;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.example.demobase", "com.example.demo"})
-@EnableJpaRepositories(basePackages = {"com.example.demobase"})
+@SpringBootApplication(scanBasePackages = "com.example")
+//@ComponentScan(basePackages = {"com.example.demobase", "com.example.demo"})
+//@EnableJpaRepositories(basePackages = {"com.example.demobase"})
 public class DemoAppApplication {
     private static Logger log = LoggerFactory.getLogger(DemoAppApplication.class);
     public static void main(String[] args) {
--- demo-app/src/main/java/com/example/demoapp/TestController.java
+++ demo-app/src/main/java/com/example/demoapp/TestController.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.example.demobase.MemberRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
